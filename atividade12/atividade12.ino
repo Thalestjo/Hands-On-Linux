3c95c50f5e70a279f006a7a025a95d6e63de8e8f
DerioEle <deriooliveira3@gmail.com>
2026-05-13 19:34:05 -0400

    feat: Atividade 1.2 - LED funcionando

--- atividade12/atividade12.ino
+++ atividade12/atividade12.ino
@@ -1,13 +1,20 @@
 #define ledpin 22
 
+void blink_led(){
+    for (int i=0; i<5; i++){
+      digitalWrite(ledpin, HIGH);
+      delay(2000);
+      digitalWrite(ledpin, LOW);
+      delay(2000);
+    }
+  }
+
 void setup(){
   Serial.begin(9600);
   pinMode(ledpin, OUTPUT);
+  blink_led();
   }
 
+
 void loop(){
-  digitalWrite(ledpin, HIGH);
-  delay(2000);
-  digitalWrite(ledpin, LOW);
-  delay(2000);
   }
